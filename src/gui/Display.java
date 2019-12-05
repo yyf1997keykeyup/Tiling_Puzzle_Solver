fedaee9fbc9c75d0fc65238d8f93422d59932fb8
Renol <Renol@yanxixi-3.local>
2019-12-04 20:22:40 -0500

    first update

---
+++ src/gui/Display.java
@@ -0,0 +1,12 @@
+package gui;
+
+import javax.swing.*;
+
+public class GUI {
+    private JPanel panel1;
+    private JLabel TestJLabel;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
